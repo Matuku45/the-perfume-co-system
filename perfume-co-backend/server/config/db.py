5accf8363ff2d612ad94201b2fa0f9932bd06d1f
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2026-01-11 13:35:15 +0200

    fixed the depractation error for database

--- perfume-co-backend/server/config/db.py
+++ perfume-co-backend/server/config/db.py
@@ -1,10 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from dotenv import load_dotenv
+
+load_dotenv()  # load .env file
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine)
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set in .env")
 
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
